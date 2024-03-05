9b5ff417d02bd3d2df765d973230a1e0e04a3be1
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-05 15:57:24 +0100

    feat: import and execute the express app

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Import http 
 const http = require('http');
 
-// Create a simple server
-const server = http.createServer((req, res) => {
-    res.end('Hello World!');
-})
+// Import the express app
+const app = require('./app');
+
+// Set the port of the server
+app.set('port', process.env.PORT || 3000);
+
+// Create a simple server which execute the express app
+const server = http.createServer(app);
 
 // Listen to the server
 server.listen(process.env.PORT || 3000);
