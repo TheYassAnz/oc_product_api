2c6018bceed6d2ed81998a8ad0d165a6e6ac8fbb
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-06 17:05:32 +0100

    feat: GET /api/products

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 // Import Mongoose
 const mongoose = require('mongoose');
 
+// Import Product model
+const Product = require('./models/Product');
+
 // Connect to the database 
 mongoose.connect('mongodb+srv://ysma:zQQ5jj82GDAP2sen@ocexpressexercice.xtyis6e.mongodb.net/?retryWrites=true&w=majority&appName=ocExpressExercice',
     {
@@ -16,9 +19,13 @@ mongoose.connect('mongodb+srv://ysma:zQQ5jj82GDAP2sen@ocexpressexercice.xtyis6e.
 // Create an express app
 const app = express();
 
-// First middleware will be called for every request
-app.use((req, res, next) => {
-    res.json({ message: 'Hello World!' });
+// GET /api/products route 
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+        // return a json object with the products
+        .then(products => res.status(200).json(products))
+        // return the error object if there is an error
+        .catch(error => res.status(500).json({ error }));
 });
 
 // Export the express app
