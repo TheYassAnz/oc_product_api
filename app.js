1ce1ad4c43dcfbfc810c4f69104602c062423e8f
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-06 16:48:25 +0100

    feat: mongoose installed and connected

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // Import express
 const express = require('express');
 
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Connect to the database 
+mongoose.connect('mongodb+srv://ysma:zQQ5jj82GDAP2sen@ocexpressexercice.xtyis6e.mongodb.net/?retryWrites=true&w=majority&appName=ocExpressExercice',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // Create an express app
 const app = express();
 
