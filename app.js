3989b6913e871c4588ba557b59e3f95da2c4e2ed
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-06 10:56:24 +0100

    feat : simple middleware wich returns JSON object

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const express = require('express');
 // Create an express app
 const app = express();
 
+// First middleware will be called for every request
+app.use((req, res, next) => {
+    res.json({ message: 'Hello World!' });
+});
+
 // Export the express app
 module.exports = app;
