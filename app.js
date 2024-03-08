6c8b0d3b18488d66c1ab4950c63834f0b2f4dcf2
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-03-08 22:28:17 +0100

    fix: CORS headers

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ mongoose.connect('mongodb+srv://ysma:zQQ5jj82GDAP2sen@ocexpressexercice.xtyis6e.
 // Create an express app
 const app = express();
 
+// Add CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 // GET /api/products route 
 app.get('/api/products', (req, res, next) => {
     Product.find()
